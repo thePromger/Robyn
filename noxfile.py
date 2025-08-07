f4d6f1af279483339b78be5c431d8ea71a320a97
Promger <96533520+thePromger@users.noreply.github.com>
2025-08-07 11:29:37 +0000

    t1

--- noxfile.py
+++ noxfile.py
@@ -4,20 +4,19 @@ import nox
 
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
-def tests(session):
-    # session.run("pip", "install", "poetry==1.3.0")
-    
-    # uv_export = "uv export --locked --no-hashes --group dev --group test -o deps.txt"
-    # uv_export = "uv export --locked --no-hashes --group dev --group test -o deps.txt --no-install-project"
+def tests(session: nox.Session):
+    # print(session.python)
+    # return
+    session.install("uv", "maturin", "-U")
+    # uv_export = "uv export --no-hashes --group dev --group test -o deps.txt --no-install-project"
     uv_export = "uv export --no-hashes --group dev --group test -o deps.txt --no-install-project"
     uv_pip_install = "uv pip sync deps.txt"
 
-    session.run("pip", "install", "uv")
     session.run(*uv_export.split(" "))
     session.run(*uv_pip_install.split(" "))
-    session.run("uv", "pip", "install", "maturin")
 
-    maturin_build = "maturin build -i python --out dist"
+    # maturin_build = "maturin develop"
+    maturin_build = "maturin pep517 build-wheel --editable -o dist"
 
     if sys.platform == "darwin":
         session.run("rustup", "target", "add", "x86_64-apple-darwin")
@@ -26,9 +25,6 @@ def tests(session):
 
     session.run(*maturin_build.split(" "))
     session.run("uv", "pip", "install", "--no-index", "--find-links=dist/", "robyn")
-    session.run("whereis", "pytest")
-    session.run("which", "pytest")
-    # session.run("uv", "run", "pytest")
     session.run("pytest")
 
 
